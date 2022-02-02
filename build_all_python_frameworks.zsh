545d9da8d35970d8565e455bf97ddbcf76472b83
Nate Walck <github@natewalck.com>
2022-02-02 16:49:33 -0500

    Update to Python 3.9.10 and update libs (#36)
    * Update to Python 3.9.10 and update libs
    
    * Fix tokenize-rt version as 4.2.0 was yanked
    
    * PyObjC doesn't seem universal anymore, trying to force a build
    
    * Let's upgrade to latest release of PyObjC
    
    * Update release notes to reflect PyObjC change

--- build_all_python_frameworks.zsh
+++ build_all_python_frameworks.zsh
@@ -11,4 +11,3 @@ SIGNING_IDENTITY="Developer ID Installer: Clever DevOps Co. (9GQZ7KUFR6)"
 "$TOOLSDIR/build_python_framework_pkgs.zsh" minimal ${SIGNING_IDENTITY}
 "$TOOLSDIR/build_python_framework_pkgs.zsh" no_customization ${SIGNING_IDENTITY}
 "$TOOLSDIR/build_python_framework_pkgs.zsh" recommended ${SIGNING_IDENTITY}
-"$TOOLSDIR/build_python_framework_pkgs.zsh" opinionated ${SIGNING_IDENTITY}
